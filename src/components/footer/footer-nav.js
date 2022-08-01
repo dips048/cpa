6d0f5f4bef360156683c56c9d5175efe4743c7f2
dips048 <dipeshpatel048@gmail.com>
2022-08-02 00:01:19 +0530

    + footer nav component used at 3 footer container

---
+++ src/components/footer/footer-nav.js
@@ -0,0 +1,65 @@
+import * as React from "react"
+import { container, navLinks, linkConatainer } from "./footer-nav.module.css"
+import { Link } from "gatsby"
+
+const data = [
+  {
+    heading: "Resources",
+    links: [
+      "Calculators",
+      "Client Portal",
+      "SecureSend",
+      "Internet Links",
+      "Newsletter",
+    ],
+  },
+  {
+    heading: "Resources",
+    links: [
+      "Track Your Refund",
+      "Tax Due Dates",
+      "Tax Rates",
+      "Record Retention Guide",
+      "State Tax Forms",
+    ],
+  },
+  {
+    heading: "Resources",
+    links: [
+      "Company Name",
+      "59 Rathe Road",
+      "Colchester, VT 05408",
+      "(800) 896-4500",
+      "support@cpasitesolutions.com",
+    ],
+  },
+]
+
+const FooterNav = () => {
+  return (
+    <div className={container}>
+      {data.map((card, i) => (
+        <div className={linkConatainer}>
+          <ul className={navLinks}>
+            <h4>{card.heading}</h4>
+            {card.links.map((link, j) => (
+              <li>
+                <Link to={"/" + link}>{link}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <div>
+        <ul>
+          <li>© Company Name 2022</li>
+          <li>Site Map</li>
+          <li>Privacy Policy</li>
+          <li>Disclaimer</li>
+        </ul>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+export default FooterNav
