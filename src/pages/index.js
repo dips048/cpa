37f74c915b8914f25a272d44459801fe11c9cab7
dips048 <dipeshpatel048@gmail.com>
2022-08-02 00:13:41 +0530

    index + home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <div>Hello world!</div>
+    </Layout>
+  )
 }
+
+export default IndexPage
