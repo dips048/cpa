c87f55c39a60f7120b299320027fd7bf7fc6850c
dips048 <dipeshpatel048@gmail.com>
2022-08-02 00:00:44 +0530

    + newsletter component used at 2 footer container

---
+++ src/components/footer/newsletter.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { container } from "./newsletter.module.css"
+
+const Newsletter = () => {
+  return (
+    <div className={container}>
+      <h2>Newsletter</h2>
+      <p>
+        Subscribe to our newsletter to receive news, updates, and valuable tips.
+      </p>
+      <div>
+        <form>
+          <input type="text" name="email" placeholder="Email" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  )
+}
+export default Newsletter
