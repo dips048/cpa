1e1787755e2828a9a826603fba6110d892d7f262
dips048 <dipeshpatel048@gmail.com>
2022-08-02 23:51:17 +0530

    + Heading component

---
+++ src/components/heading.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { pattern1, globe, globe1, globe2, globe3, globe4 } from "./heading.module.scss"
+import { Container } from "react-bootstrap"
+
+const Heading = ({ title }) => {
+  return (
+    <div style={{ backgroundColor: "#2d82a1", color: "#fff" }}>
+      <div className={pattern1}></div>
+      <div className={globe}>
+        <div className={globe1}></div>
+        <div className={globe2}></div>
+        <div className={globe3}></div>
+        <div className={globe4}></div>
+      </div>
+      <Container>
+        <div className="row py-5">
+          <h1>{title}</h1>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Heading
