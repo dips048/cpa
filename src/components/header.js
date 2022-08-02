7a449301ea8100f6ac93cca68b50dd3106c5fbaf
dips048 <dipeshpatel048@gmail.com>
2022-08-02 23:53:23 +0530

    + added sm-header and level1 navgation dropdowns

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,43 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import {
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  navLinkContainer,
-} from "./header.module.scss"
 import Dropdown from "./dropdown"
+import { Container } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faLock,
+  faFeed,
+  faEnvelope,
+  faPhone,
+  faNavicon
+} from "@fortawesome/free-solid-svg-icons"
 
 const Header = ({ companyName, menuLinks }) => {
   return (
-    <nav className={navLinkContainer}>
-      <h1>{companyName}</h1>
-      <Dropdown menuLinks={menuLinks}></Dropdown>
-      {/* <ul>
-        {menuLinks.map(menuLink => (
-          <li>
-            <Link to={menuLink.url} className={navLinkText}>
-              {menuLink.name}
-            </Link>
-            <ul>
-              {menuLink.subLinks?.map(link => (
-                <Link to={menuLink.url + link.url} className={navLinkText}>
-                  {link.name}
-                </Link>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul> */}
-
-      {/* <ul className={navLinks}>
-        {menuLinks.map(menuLink => (
-          <li className={navLinkItem} key={menuLink.name}>
-            <Link to={menuLink.url} className={navLinkText}>
-              {menuLink.name}
-            </Link>
-          </li>
-        ))}
-      </ul> */}
-    </nav>
+    <Container>
+      <div className="d-flex justify-content-between d-lg-none py-2">
+        <FontAwesomeIcon className="flex-grow-1" icon={faLock}></FontAwesomeIcon>
+        <FontAwesomeIcon className="flex-grow-1" icon={faFeed}></FontAwesomeIcon>
+        <FontAwesomeIcon className="flex-grow-1" icon={faEnvelope}></FontAwesomeIcon>
+        <FontAwesomeIcon className="flex-grow-1" icon={faPhone}></FontAwesomeIcon>
+        <FontAwesomeIcon className="flex-grow-1" icon={faNavicon}></FontAwesomeIcon>
+      </div>
+      <div className="d-none d-lg-flex justify-content-lg-between py-2">
+        <div>
+          <button>Login</button>
+          <button>Subscribe</button>
+        </div>
+        <h3>
+          <a href="tel:(800) 896-4500" className="strong lead">
+            Call Us (987) 654 4321
+          </a>
+        </h3>
+      </div>
+      <nav className="d-flex justify-content-center justify-content-lg-between py-2">
+        <h1>{companyName}</h1>
+        <div className="d-none d-lg-flex">
+          <Dropdown menuLinks={menuLinks}></Dropdown>
+        </div>
+      </nav>
+    </Container>
   )
 }
 
