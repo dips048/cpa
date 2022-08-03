65cee97dfbd5f0a9df8c0eb70d61128cb9fc3e37
dips048 <dipeshpatel048@gmail.com>
2022-08-03 23:57:28 +0530

    + login Modal

---
+++ src/components/login-modal.js
@@ -0,0 +1,39 @@
+import * as React from "react"
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+import Form from "react-bootstrap/Form"
+
+const LoginModal = props => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="loginForm.ControlInput1">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Login</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default LoginModal
