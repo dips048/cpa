e71052106e2ff3c30510a61d7979d55ec3e23ce3
dips048 <dipeshpatel048@gmail.com>
2022-08-03 23:59:29 +0530

    + added 404 page for build

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 
-export default () => (
+export default NotFoundPage = () => (
   <Layout>
     <div >
       <h1 >Page not found</h1>
