1f3105df6a2e7859979f163885ffb12d54c88239
dips048 <dipeshpatel048@gmail.com>
2022-08-03 23:57:56 +0530

    + newsletter modal

---
+++ src/components/newsletter-modal.js
@@ -0,0 +1,21 @@
+import * as React from "react"
+import Modal from "react-bootstrap/Modal"
+import Newsletter from "./footer/newsletter"
+
+const NewsletterModal = props => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton></Modal.Header>
+      <Modal.Body>
+        <Newsletter />
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default NewsletterModal
