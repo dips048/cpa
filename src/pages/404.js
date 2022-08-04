f5da0610be5a402af37fa2453403d0ea45619dc7
dips048 <dipeshpatel048@gmail.com>
2022-08-04 19:25:06 +0530

    service sub pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,15 @@
 import React from "react"
-import Layout from "../components/layout"
 import { Link } from "gatsby"
+import { Container } from "react-bootstrap"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Container>
     <div>
       <h1>Page not found</h1>
       <p>The page you are looking for does not exists.</p>
       <Link to="/">Home</Link>
     </div>
-  </Layout>
+  </Container>
 )
 
 export default NotFoundPage
