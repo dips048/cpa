821a35022778ef6c2bb13556ccd0e6ab5ef1bc5d
dips048 <dipeshpatel048@gmail.com>
2022-08-09 17:18:36 +0530

    + dropdown  navigation modification

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -4,7 +4,9 @@ import {
   navLinkText,
   linkContainer,
   linkItem,
-  subLinkContainer,
+  subLinkContainer1,
+  subLinkContainer2,
+  subLinkContainer3
 } from "./dropdown.module.scss"
 
 const Dropdown = ({ menuLinks }) => (
@@ -14,13 +16,13 @@ const Dropdown = ({ menuLinks }) => (
         <Link to={link1.url} className={navLinkText}>
           {link1.name}
         </Link>
-        <ul className={subLinkContainer} style={{ listStyle: "none" }}>
+        <ul className={subLinkContainer1} >
           {link1.subLinks?.map((link2, j) => (
             <li key={link2.url + "-" + j}>
               <Link to={link1.url + link2.url} className={navLinkText}>
                 {link2.name}
               </Link>
-              <ul style={{ listStyle: "none" }}>
+              <ul className={subLinkContainer2} >
                 {link2.subLinks?.map((link3, k) => (
                   <li key={link3.url + "-" + k}>
                     <Link
@@ -29,7 +31,7 @@ const Dropdown = ({ menuLinks }) => (
                     >
                       {link3.name}
                     </Link>
-                    <ul style={{ listStyle: "none" }}>
+                    <ul className={subLinkContainer3} >
                       {link3.subLinks?.map((link4, l) => (
                         <li key={link4.url + "-" + l}>
                           <Link
