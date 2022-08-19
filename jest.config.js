12ac2e51a6605e122c248d2e159e48c34e335ae3
dips048 <dipeshpatel048@gmail.com>
2022-08-19 16:47:21 +0530

    + @testing-library/react + jest-environment-jsdom

--- jest.config.js
+++ jest.config.js
@@ -21,4 +21,6 @@ module.exports = {
       url: `http://localhost`,
     },
     setupFiles: [`<rootDir>/loadershim.js`],
+    testEnvironment: `jsdom`,
+    setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
   }
--- src/components/__tests__/callLink.js
+++ src/components/__tests__/callLink.js
@@ -1,6 +1,16 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import CallLink from "../callLink"
+import { render } from "@testing-library/react"
+
+
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+test("Displays the correct title", () => {
+  const { getByTestId } = render(<Title />)
+  // Assertion
+  expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
+  // --> Test will pass
+})
 
 describe("CallLink", () => {
   it("renders correctly", () => {
